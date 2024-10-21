9486109229ab98e4b94140a3154f09dd7dc6e082
fadelanumah <fadelanumah@gmail.com>
2024-10-22 06:34:17 +0700

    feat(productCategory): Add CRUD Resource

--- src/modules/productCategories/product-category.controller.ts
+++ src/modules/productCategories/product-category.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Post, Request } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, Request, UseGuards } from "@nestjs/common";
 import { ProductCategoryService } from "./product-category.service";
 import { CreateProductCategoryDto } from "./dtos/create-product-category.dto";
-import { ExpressRequestWithUser } from "../users/interfaces/express-request-with-user.interface";
 import { ProductCategory } from "@prisma/client";
+import { Public } from "src/common/decorators/public.decorator";
+import { QueryPaginationDto } from "src/common/dtos/query-pagination.dto";
+import { PaginateOutput } from "src/common/utils/pagination.utils";
+import { UpdateProductCategoryDto } from "./dtos/update-product-category.dto";
 
 @Controller('product-categories')
 export class ProductCategoryController {
@@ -11,7 +14,34 @@ export class ProductCategoryController {
     @Post()
     async createPost(
         @Body() createProductCategoryDto: CreateProductCategoryDto,
-    ): Promise <ProductCategory> {
+    ): Promise<ProductCategory> {
         return this.productCategoryService.createProductCategory(createProductCategoryDto)
+    }
+
+    @Public()
+    @Get()
+    getAllProductCategories(
+        @Query() query?: QueryPaginationDto
+    ): Promise<PaginateOutput<ProductCategory>> {
+        return this.productCategoryService.getAllProductCategory(query)
+    }
+
+    @Public()
+    @Get(':id')
+    getPostById(@Param('id', ParseIntPipe) id: number): Promise<ProductCategory> {
+        return this.productCategoryService.getProductCategoryById(id)
+    }
+
+    @Patch(':id')
+    async updateProductCategory(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() updateProductCategoryDto: UpdateProductCategoryDto
+    ): Promise<ProductCategory> {
+        return this.productCategoryService.updateProductCategory(+id, updateProductCategoryDto)
+    }
+
+    @Delete(':id')
+    async deleteProductCategory(@Param('id', ParseIntPipe) id: number): Promise<string> {
+        return this.productCategoryService.deleteProductCategory(+id)
     }
 }
--- src/modules/productCategories/product-category.service.ts
+++ src/modules/productCategories/product-category.service.ts
@@ -1,7 +1,10 @@
-import { ConflictException, Injectable } from "@nestjs/common";
+import { ConflictException, HttpException, Injectable, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "src/core/services/prisma.service";
 import { CreateProductCategoryDto } from "./dtos/create-product-category.dto";
 import { ProductCategory } from "@prisma/client";
+import { QueryPaginationDto } from "src/common/dtos/query-pagination.dto";
+import { paginate, paginateOutput, PaginateOutput } from "src/common/utils/pagination.utils";
+import { UpdateProductCategoryDto } from "./dtos/update-product-category.dto";
 
 @Injectable()
 export class ProductCategoryService {
@@ -19,6 +22,76 @@ export class ProductCategoryService {
         } catch (error) {
             if (error.code === 'P2002') {
                 throw new ConflictException('Category already exists')
+            }
+        }
+    }
+
+    async getAllProductCategory(query?: QueryPaginationDto): Promise<PaginateOutput<ProductCategory>> {
+        const [productCategories, total] = await Promise.all([
+            await this.prisma.productCategory.findMany({
+                ...paginate(query)
+            }),
+            await this.prisma.productCategory.count()
+        ])
+
+        return paginateOutput<ProductCategory>(productCategories, total, query)
+    }
+
+    async getProductCategoryById(id: number): Promise<ProductCategory> {
+        try {
+            const productCategory = await this.prisma.productCategory.findUniqueOrThrow({
+                where: { id }
+            })
+
+            return productCategory
+        } catch (error) {
+            if (error.code === 'P2025') {
+                throw new NotFoundException(`Product category with id ${id} not found`)
+            }
+
+            throw new HttpException(error, 500)
+        }
+    }
+
+    async updateProductCategory(id: number, updateProductCateogryDto: UpdateProductCategoryDto): Promise<ProductCategory> {
+        try {
+            await this.prisma.productCategory.findUniqueOrThrow({
+                where: { id }
+            })
+
+            const updateProductCateogry = await this.prisma.productCategory.update({
+                where: { id },
+                data: {
+                    ...updateProductCateogryDto
+                }
+            })
+
+            return updateProductCateogry
+        } catch (error) {
+            if (error.code === 'P2025') {
+                throw new NotFoundException(`Product category with id ${id} not found`)
+            }
+
+            if (error.code === 'P2002') {
+                throw new ConflictException('Product category already exists')
+            }
+        }
+    }
+
+    async deleteProductCategory(id: number): Promise<string> {
+        try {
+            const productCategory = await this.prisma.productCategory.findUniqueOrThrow({
+                where: { id }
+            })
+
+            await this.prisma.productCategory.delete({
+                where: { id }
+            })
+
+            return `Product category with id ${productCategory.id} deleted`
+        } catch (error) {
+            if (error.code === 'P2025') {
+                throw new NotFoundException(`Product category with id ${id} not found`)
             }
         }
     }
