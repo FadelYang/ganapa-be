84413f73ed682f274dfe82805c060acafaef763e
fadelanumah <fadelanumah@gmail.com>
2024-10-22 12:23:12 +0700

    feat(product): Add Create and Update DTO

---
+++ src/modules/products/dtos/create-product.dto.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty, IsNumber } from "class-validator";
+
+export class CreateProductDto {
+    @IsNotEmpty()
+    name: string
+
+    @IsNotEmpty()
+    description: string
+
+    @IsNotEmpty()
+    @IsNumber()
+    price: number
+
+    @IsNotEmpty()
+    @IsNumber()
+    stock: number
+
+    productCategoryId: number
+}
