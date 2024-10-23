378da877de759f3271a4fb8a55e9d56cdad2b978
fadelanumah <fadelanumah@gmail.com>
2024-10-23 16:14:51 +0700

    feat: Add Role Guard and Decorator

---
+++ src/common/roles.guard.ts
@@ -0,0 +1,26 @@
+import { CanActivate, ExecutionContext } from "@nestjs/common"
+import { Reflector } from "@nestjs/core"
+import { JwtService } from "@nestjs/jwt"
+import { UserRole } from "@prisma/client";
+import { ROLES_KEY } from "./decorators/roles-decorator";
+
+export class RolesGuard implements CanActivate {
+    constructor(private reflector: Reflector, private jwtService: JwtService) { }
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(ROLES_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+
+        if (!requiredRoles) {
+            return true
+        }
+
+        const request = context.switchToHttp().getRequest()
+        const token = request.headers.authorization.split(' ')[1]
+        const user = await this.jwtService.verifyAsync(token)
+
+        return requiredRoles.includes(user.role)
+    }
+}
